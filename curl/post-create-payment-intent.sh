efb513ed633206c8d06a146ff597660c222bb13a
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-07-10 18:32:38 -0300

    return total_in_cents

--- curl/post-create-payment-intent.sh
+++ curl/post-create-payment-intent.sh
@@ -1,7 +1,11 @@
 API="http://localhost:1337/orders/create-payment-intent"
 TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTYyNTk0ODU3MSwiZXhwIjoxNjI4NTQwNTcxfQ.aUM4SRQf710rQn2OUj5PBt7X7Ur7titC8Lr_0rYxFpU"
-DATA='{"cart":[{"id":1}]}'
-
+#valid id
+#DATA='{"cart":[{"id":1}]}'
+#invalid id
+#DATA='{"cart":[{"id":989888}]}'
+#multiples ids
+DATA='{"cart":[{"id":1},{"id":2},{"id":3}]}'
 echo `curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer ${TOKEN}" -d ${DATA} ${API}` | json_pp
 
 
