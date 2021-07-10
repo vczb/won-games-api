6b78db4ab0c2ee5b05f47a716cdd0d4975501d3a
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-07-10 19:18:51 -0300

    create paymentIntent on Stripe

--- scripts/post-create-payment-intent.sh
+++ scripts/post-create-payment-intent.sh
@@ -5,9 +5,9 @@ TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTYyNTk0ODU3MSwi
 #invalid id
 #DATA='{"cart":[{"id":989888}]}'
 #multiple valid ids
-# DATA='{"cart":[{"id":1},{"id":2},{"id":3}]}'
+DATA='{"cart":[{"id":1},{"id":2},{"id":3}]}'
 #free games ids
-DATA='{"cart":[{"id":100},{"id":124}]}'
+# DATA='{"cart":[{"id":100},{"id":124}]}'
 echo `curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer ${TOKEN}" -d ${DATA} ${API}` | json_pp
 
 
