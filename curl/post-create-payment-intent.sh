780f218364d711f9097e0ca02e028d13b2a20617
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-07-10 18:48:29 -0300

    return freeGames

--- curl/post-create-payment-intent.sh
+++ curl/post-create-payment-intent.sh
@@ -4,8 +4,10 @@ TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTYyNTk0ODU3MSwi
 #DATA='{"cart":[{"id":1}]}'
 #invalid id
 #DATA='{"cart":[{"id":989888}]}'
-#multiples ids
-DATA='{"cart":[{"id":1},{"id":2},{"id":3}]}'
+#multiple valid ids
+# DATA='{"cart":[{"id":1},{"id":2},{"id":3}]}'
+#free games ids
+DATA='{"cart":[{"id":100},{"id":124}]}'
 echo `curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer ${TOKEN}" -d ${DATA} ${API}` | json_pp
 
 
