22bcc52927e35bc5911d8b842f733d831c2e6b80
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-09-12 18:18:40 -0300

    send email template

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,34 +1,35 @@
-'use strict';
+"use strict";
 
-const stripe = require('stripe')(process.env.STRIPE_KEY)
-const { sanitizeEntity } = require('strapi-utils')
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const { sanitizeEntity } = require("strapi-utils");
+const orderTemplate = require("../../../config/email-templates/order");
 
 module.exports = {
   createPaymentIntent: async (ctx) => {
-    const { cart } = ctx.request.body
+    const { cart } = ctx.request.body;
 
-    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart)
+    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart);
 
-    const games = await strapi.config.functions.cart.cartItems(cartGamesIds)
+    const games = await strapi.config.functions.cart.cartItems(cartGamesIds);
 
-    if(!games.length){
-      ctx.response.status = 404
+    if (!games.length) {
+      ctx.response.status = 404;
       return {
-        error: "No valid games found!"
-      }
+        error: "No valid games found!",
+      };
     }
 
-    const total = await strapi.config.functions.cart.total(games)
+    const total = await strapi.config.functions.cart.total(games);
 
-    if(total === 0){
+    if (total === 0) {
       return {
-        freeGames: true
-      }
+        freeGames: true,
+      };
     }
 
     try {
       const paymentIntent = await stripe.paymentIntents.create({
-        amount: total * 100,
+        amount: total,
         currency: "usd",
         metadata: { cart: JSON.stringify(cartGamesIds) },
       });
@@ -39,40 +40,65 @@ module.exports = {
         error: err.raw.message,
       };
     }
-
   },
 
   create: async (ctx) => {
-    const { cart, paymentIntentId, paymentMethod } = ctx.request.body
+    const { cart, paymentIntentId, paymentMethod } = ctx.request.body;
 
-    const token = await strapi.plugins["users-permissions"].services.jwt.getToken(ctx)
+    const token = await strapi.plugins[
+      "users-permissions"
+    ].services.jwt.getToken(ctx);
 
-    const userId = token.id
+    const userId = token.id;
 
-    const userInfo = await strapi.query("user", "users-permissions").findOne({
-      id: userId
-    })
+    const userInfo = await strapi
+      .query("user", "users-permissions")
+      .findOne({ id: userId });
 
+    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart);
 
-    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart)
+    const games = await strapi.config.functions.cart.cartItems(cartGamesIds);
 
-    const games = await strapi.config.functions.cart.cartItems(cartGamesIds)
+    const total_in_cents = await strapi.config.functions.cart.total(games);
 
-    const total_in_cents = await strapi.config.functions.cart.total(games)
+    let paymentInfo;
+    if (total_in_cents !== 0) {
+      try {
+        paymentInfo = await stripe.paymentMethods.retrieve(paymentMethod);
+      } catch (err) {
+        ctx.response.status = 402;
+        return { error: err.message };
+      }
+    }
 
     const entry = {
       total_in_cents,
       payment_intent_id: paymentIntentId,
-      card_brand: null,
-      card_last4: null,
+      card_brand: paymentInfo?.card?.brand,
+      card_last4: paymentInfo?.card?.last4,
       user: userInfo,
-      games
-    }
+      games,
+    };
 
-    const entity = await strapi.services.order.create(entry)
+    const entity = await strapi.services.order.create(entry);
 
+    await strapi.plugins.email.services.email.sendTemplatedEmail(
+      {
+        to: userInfo.email,
+        from: "no-reply@wongames.com",
+      },
+      orderTemplate,
+      {
+        user: userInfo,
+        payment: {
+          total: `$ ${total_in_cents / 100}`,
+          card_brand: entry.card_brand,
+          card_last4: entry.card_last4,
+        },
+        games,
+      }
+    );
 
-    return sanitizeEntity(entity, { model: strapi.models.order })
-
-  }
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
 };
